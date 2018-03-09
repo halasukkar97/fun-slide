c3187bcbd6feac8b2e29f513963fe5f0959f68c2
hela sukkar <32265046+halasukkar97@users.noreply.github.com>
2018-03-09 04:15:03 +0000

    no message

---
+++ Assets/scripts/PlayerMovment.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovment : MonoBehaviour {
+
+    private Rigidbody rigibody;
+    [SerializeField]
+    private float speed;
+    
+	// Use this for initialization
+	void Start () {
+        rigibody = this.GetComponent<Rigidbody>();
+        rigibody.velocity = transform.forward * speed;
+
+    }
+	
+}
